9b9f9f2e42c7e2a7a6a85cc9afb16d0c295ad76f
nancy0786 <the.anubhav786@gmail.com>
2025-08-15 03:04:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ FORCE_JOIN_CHANNEL = os.getenv("FORCE_JOIN_CHANNEL", "@playhubby").strip()
 ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").replace(" ", "").split(",") if x]
 LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", "0").strip())
 WELCOME_FILE = os.path.join("data", "welcome.txt")
+
+SPONSOR_BOT_USERNAME = os.getenv("SPONSOR_BOT_USERNAME", "").strip()
+SPONSOR_BOT_ID = int(os.getenv("SPONSOR_BOT_ID", "0").strip())
