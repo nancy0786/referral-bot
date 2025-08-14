a125c2385628143ec318e2cc3693231904ce1104
nancy0786 <the.anubhav786@gmail.com>
2025-08-15 02:49:31 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+FORCE_JOIN_CHANNEL = os.getenv("FORCE_JOIN_CHANNEL", "@playhubby").strip()
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").replace(" ", "").split(",") if x]
+LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", "0").strip())
+WELCOME_FILE = os.path.join("data", "welcome.txt")
