2dd9b772787bc054a56fef5a14153fba3cc192d4
nancy0786 <the.anubhav786@gmail.com>
2025-08-21 15:56:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import os
 import logging
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ChannelPostHandler
-
 from telegram.ext import (
     Application,
     CommandHandler,
