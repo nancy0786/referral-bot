eb7114553ef658d5bf070df88e6fe27dd6bd97ee
nancy0786 <the.anubhav786@gmail.com>
2025-08-22 20:21:37 +0530

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -1,7 +1,7 @@
 # utils/checks.py
 
 from handlers.force_join import is_member, prompt_join
-from handlers.sponsor_verify import ask_sponsor_verification
+from handlers.sponsor_verify import auto_verify_sponsor
 from utils.db import get_user, save_user
 
 async def ensure_access(update, context):
